91d0a618e246b7e073f5b4ca7cca3fc7363a90d9
AlthafHussainSelenium <althafhussain.pcet@gmail.com>
2025-01-19 11:15:25 +0530

    git add new script

--- 04-Variable-Read.sh
+++ 04-Variable-Read.sh
@@ -6,3 +6,7 @@ echo "Please enter your name: " # User has to enter the text at runtime
 read NAME # Here NAME will be variable
 
 echo "Name entered: $NAME"
+
+echo "Please enter your password: "
+
+read -s PASSWORD
