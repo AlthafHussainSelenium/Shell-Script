33d1bf793b4b8de9766f40c64aa71c6b10c48b21
AlthafHussainSelenium <althafhussain.pcet@gmail.com>
2025-01-19 10:51:08 +0530

    Added

--- 02-Variables.sh
+++ 02-Variables.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-VARIABLE1="Althaf"
+VARIABLE1="Althaf" #no space between =
 VARIABLE2="Zuveriya"
 VARIABLE3="Umar Khalid"
 
 echo "$VARIABLE2 is $VARIABLE1 daughter"
-echo "$VARIABLE1 is $VARIABLE2 father"
+echo "${VARIABLE1} is $VARIABLE2 father" #we can give the variable like this also
 echo "$VARIABLE3 is $VARIABLE1 son"
 echo "$VARIABLE1 is $VARIABLE3 father"
