089744d867d76233efd2bfbf4019f1810574fb62
AlthafHussainSelenium <althafhussain.pcet@gmail.com>
2025-01-19 10:55:47 +0530

    Added runtime Arguments script

---
+++ 03-RunTime-Arguments.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+FATHER=$1
+DAUGHTER=$2
+SON=$3
+
+
+FATHER=$1
+echo "$DAUGHTER is $FATHER daughter"
+echo "${FATHER} is $DAUGHTER father" #we can give the variable like this also
+echo "$SON is $FATHER son"
+echo "$FATHER is $SON father"
