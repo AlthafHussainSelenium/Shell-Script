a1bee3b004757638323fd5c71821df96b3cfe15f
AlthafHussainSelenium <althafhussain.pcet@gmail.com>
2025-01-19 11:06:09 +0530

    added read command scripts

---
+++ 04-Variable-Read.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# This will hide the inputs to make secure
+echo "Please enter your name: " # User has to enter the text at runtime
+
+read NAME # Here NAME will be variable
+
+echo "Name entered: $NAME"
