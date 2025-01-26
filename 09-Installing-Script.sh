8e8619856865b620e416449aa7a40056edca5f61
AlthafHussainSelenium <althafhussain.pcet@gmail.com>
2025-01-26 10:10:43 +0530

    added fix

--- 09-Installing-Script.sh
+++ 09-Installing-Script.sh
@@ -6,8 +6,20 @@
 # else thow the error message and exit the script
 
 USERID=$(id -u)
-if [ $USERID -ne 0]; then
+if [ $USERID -ne 0 ]; then
     echo "ERROR:: You must have sudo access to execute this script"
     exit 1 # other than 0
 fi
-dnf install mysql -y
+dnf list installed mysql
+
+if [ $? -ne 0 ]; then # not installed
+    dnf install mysql -y
+    if [ $? -ne 0 ]; then
+        echo "Installing MySQL ... FAILURE"
+        exit 1
+    else
+        echo "Installing MySQL ... SUCCESS"
+    fi
+else
+    echo "MySQL is already ... INSTALLED"
+fi
