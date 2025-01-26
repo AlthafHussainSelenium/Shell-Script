4b88882b56a12f2e672c77b71ce8b5e1ec631d73
AlthafHussainSelenium <althafhussain.pcet@gmail.com>
2025-01-26 10:24:30 +0530

    althaf changes

--- 09-Installing-Script.sh
+++ 09-Installing-Script.sh
@@ -10,4 +10,16 @@ if [ $USERID -ne 0]; then
     echo "ERROR:: You must have sudo access to execute this script"
     exit 1 # other than 0
 fi
-dnf install mysql -y
+dnf list installed mysql
+
+if [ $? -ne 0 ]; then # not installed
+    dnf install mysql -y
+    if [ $? -ne 0 ]; then
+        echo "Installing MySQL ... FAILURE"
+        exit 1
+    else
+        echo "Installing MySQL ... SUCCESS"
+    fi
+else
+    echo "MySQL is already ... INSTALLED"
+fi
