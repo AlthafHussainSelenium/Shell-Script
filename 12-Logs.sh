6cb89c5a1de126f87e0605839e0c9414d83afbc5
AlthafHussainSelenium <althafhussain.pcet@gmail.com>
2025-02-01 20:43:58 +0530

    added log script

---
+++ 12-Logs.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+USERID=$(id -u) # it will give us root user id
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOG_FOLDER="/var/log/shellscript-logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1) # $0 will return script name
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOG_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
+VALIDATE() {
+    if [ $? -ne 0]; then
+        echo -e "$2 ... $R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
+
+    echo "Script started execution at: $TIMESTAMP &>>$LOG_FILE_NAME"
+}
