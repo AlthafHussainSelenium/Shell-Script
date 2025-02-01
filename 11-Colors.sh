cc3cc4e84cd94980c306562b63826e48b86c90af
AlthafHussainSelenium <althafhussain.pcet@gmail.com>
2025-02-01 19:24:45 +0530

    althaf script

--- 11-Colors.sh
+++ 11-Colors.sh
@@ -7,16 +7,16 @@ Y="\e[33m]"
 
 # Method to validate
 VALIDATE() {
-    if [ $1 -ne 0 ]; then
-        echo -e $2 ... $R FAILURE $N
+    if [ $1 -ne 1 ]; then
+        echo -e "$2 ... $R FAILURE $N"
     else
-        echo -e $2 ... $G SUCCESS $N
+        echo -e "$2 ... $G SUCCESS $N"
     fi
 }
 
 # Verify Root user or not script
 if [ $USERID -ne 0 ]; then
-    echo "ERROR:: You must have sudo access to execute this script"
+    echo -e "$R ... ERROR:: You must have sudo access to execute this script"
     exit 1 # other than 0
 fi
 
