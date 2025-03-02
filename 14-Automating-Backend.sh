bc29593800fe5d2c99e67b7a6867d568f4853631
AlthafHussainSelenium <althafhussain.pcet@gmail.com>
2025-03-02 12:09:15 +0530

    added

--- 14-Automating-Backend.sh
+++ 14-Automating-Backend.sh
@@ -67,3 +67,44 @@ VALIDATE $? "Enabling nodejs:20 is"
 
 dnf install nodejs -y &>>$
 VALIDATE $? "Installing nodejs is"
+
+id expense &>>$LOG_FILE_NAME
+if [ $? -ne 0 ]; then
+    useradd expense &>>$LOG_FILE_NAME
+    VALIDATE $? "Adding expense user"
+else
+    echo -e "expense user already exists ... $Y SKIPPING $N"
+fi
+
+mkdir -p /app &>>$LOG_FILE_NAME # -p: This option tells mkdir to create the parent directories as needed. It prevents errors if the directory already exists.
+VALIDATE $? "Creating app directory"
+
+curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOG_FILE_NAME
+VALIDATE $? "Downloading backend"
+
+cd /app
+rm -rf /app/*
+
+unzip /tmp/backend.zip &>>$LOG_FILE_NAME
+VALIDATE $? "unzip backend"
+
+npm install &>>$LOG_FILE_NAME
+VALIDATE $? "Installing dependencies"
+
+cp /home/ec2-user/Shell-Script/Config/backend.service /etc/systemd/system/backend.service
+
+# Prepare MySQL Schema
+dnf install mysql -y &>>$LOG_FILE_NAME
+VALIDATE $? "Installing MySQL Client"
+
+mysql -h mysql.atzuk.space -uroot -pExpenseApp@1 </app/schema/backend.sql &>>$LOG_FILE_NAME
+VALIDATE $? "Setting up the transactions schema and tables"
+
+systemctl daemon-reload &>>$LOG_FILE_NAME
+VALIDATE $? "Daemon Reload"
+
+systemctl enable backend &>>$LOG_FILE_NAME
+VALIDATE $? "Enabling backend"
+
+systemctl restart backend &>>$LOG_FILE_NAME
+VALIDATE $? "Starting Backend"
